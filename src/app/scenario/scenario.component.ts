470df64e6fd360e5351f33ab809bf4739c711d3f
Yuvaneswaran Dhanasekaran <yuvaneswaran.dha@tigeranalytics.local>
2021-06-17 12:14:32 +0530

    before filter

--- src/app/scenario/scenario.component.ts
+++ src/app/scenario/scenario.component.ts
@@ -4,23 +4,24 @@ import {MatTableDataSource} from '@angular/material/table';
 import {SelectionModel} from '@angular/cdk/collections';
 
 export interface PeriodicElement {
-  name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  activation_type: string;
+  sku: number;
+  expect_lift: number;
+  expected_roi: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1,sku : 1235, activation_type: 'FSI', expect_lift: 13, expected_roi: 12},
+  {position: 2,sku : 1235, activation_type: 'FAI', expect_lift: 32, expected_roi:  22},
+  {position: 3,sku : 1235, activation_type: 'TPR', expect_lift: 20, expected_roi:  12},
+  {position: 4,sku : 1235, activation_type: 'Search', expect_lift: 9, expected_roi: 32},
+  {position: 5,sku : 1243, activation_type: 'FSI', expect_lift: 10 ,expected_roi: 12},
+  {position: 6,sku : 1243, activation_type: 'FAI', expect_lift: 12, expected_roi: 12},
+  {position: 7,sku : 1243, activation_type: 'TPR', expect_lift: 14, expected_roi: 14},
+  {position: 8,sku : 1243, activation_type: 'Search', expect_lift: 15, expected_roi: 22},
+  {position: 9,sku : 1246, activation_type: 'FSI', expect_lift: 18, expected_roi: 16},
+  {position: 10,sku : 1246, activation_type: 'FAI', expect_lift: 20, expected_roi:  12},
 ];
 
 @Component({
@@ -31,7 +32,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ScenarioComponent implements OnInit {
   selectedIndex:number = 0
 
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['select', 'position','sku', 'activation_type', 'expect_lift', 'expected_roi'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
   srcResult:any = ''
@@ -60,7 +61,7 @@ export class ScenarioComponent implements OnInit {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.sku + 1}`;
   }
 
   onFileSelected() {
